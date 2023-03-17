787443d6afc1bfe20e3ac160f5402c933b0e1d01
tylerspear <stylerspear@gmail.com>
2023-03-16 21:57:34 -0500

    deploy

---
+++ public/main.js
@@ -0,0 +1,32 @@
+const search = document.getElementById('search')
+const searchTerm = document.getElementById('search-term')
+const list = document.getElementById('condition-list')
+search.addEventListener('click', buildList)
+
+//fetch the data from the /api/term endpoint
+async function runSearch(e){
+    //get the input text
+    const term = searchTerm.value
+    //await the data from the endpoint, passing in the term
+    let res = await fetch(`/api/${term}`)
+    //parse the response as json
+    res = await res.json()
+    //return the array
+    return res
+}
+
+function buildList(){
+    list.textContent = ''
+    runSearch(searchTerm)
+    .then(data => {
+        data.forEach(el => {
+            buildListItem(list, el)
+        })
+    })
+}
+
+function buildListItem(parent, data){
+    let li = document.createElement('li')
+    li.innerHTML = `<span class="icd-code">${data.code}: </span><span>${data.desc}</span>`
+    parent.appendChild(li)
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+//Global Variables
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('dotenv').config()
+let db, 
+    dbName = 'icd_api'
+
+//Connect to MongoDB Database
+MongoClient.connect(process.env.DB_STRING, { useUnifiedTopology: true })
+    .then(client => {
+        db = client.db(dbName)
+        console.log(`connected to ${dbName}`)
+    })
+
+//Base API URL
+app.get('/', (request, response)=> {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const conditionName = request.params.name.toLowerCase()
+    db.collection('icd10_codes').aggregate([ { $search: { "text": { "query": conditionName, "path": "desc_lower"} } }, { $limit: 30 } ]).toArray((err, res) => {
+        if(err){
+            console.error(err)
+        } else {
+            response.send(res)
+        }
+    })
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server is running on PORT ${process.env.PORT || PORT}`)
+})
